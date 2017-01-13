ab680c5d72e1c64dd1a5df6513269f6ed2a28a7c
banana <ufozhengli@163.com>
2017-01-13 10:34:12 +0800

    a request money from b

--- app/api/v1/deals.rb
+++ app/api/v1/deals.rb
@@ -1,4 +1,42 @@
 class V1::DealApi < V1
 end
 V1::DealApi.define do
+  on get do
+
+  end
+
+  on post do
+    on 'payment', param('a'), param('b'), param('money') do |a, b, money|
+      begin
+        payment = Payment.transfer(a, b, money.to_i)
+        as_json do
+          {data: {payment: payment}}
+        end
+      rescue Exception => e
+        ap e
+        as_json 403 do
+          {err: e}
+        end
+      end
+    end
+
+    on 'repayment', param('a'), param('b'), param('money') do |a, b, money|
+      begin
+        repayment = Repayment.transfer(a, b, money.to_i)
+        as_json do
+          {data: {repayment: repayment}}
+        end
+      rescue Exception => e
+        as_json 403 do
+          {err: e}
+        end
+      end
+    end
+
+    on true do
+      as_json 403 do
+        {err: 'missing params'}
+      end
+    end
+  end
 end
--- app/model/deal.rb
+++ app/model/deal.rb
@@ -1,8 +1,29 @@
 class Deal < Sequel::Model
+  plugin :single_table_inheritance, :type
 end
 
 class Payment < Deal
+  def self.transfer(a,b,money)
+    User.db.transaction do
+      # for update is a Pessimistic Locking
+      u_a = User.for_update.first(id: a) #borrow
+      u_b = User.for_update.first(id: b) #lend
+
+      raise 'trick or treat' if money <= 0
+      if money > u_b.money
+        raise 'target does not have enough money'
+      else
+        raise 'target does not have enough money' if u_b.money < 0
+        u_a.update(:money => Sequel.+(:money, money))
+        u_b.update(money: Sequel.-(:money, money))
+        Payment.create(a: u_a.id, b: u_b.id, money: money, description: "#{u_a.username} request $#{money} from #{u_b.username}")
+      end
+    end
+  end
 end
 
 class Repayment < Deal
+  def self.transfer(a,b,money)
+
+  end
 end
